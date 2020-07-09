605f1a307b0bf3e9448aad7a4604356c669b1b1b
Ryan Spangler <ryan.spangler@gmail.com>
2020-07-08 18:30:23 -0700

    run each deriver one at a time

--- vivarium/processes/tree_mass.py
+++ vivarium/processes/tree_mass.py
@@ -59,9 +59,6 @@ class TreeMass(Deriver):
 
     def next_update(self, timestep, states):
         initial_mass = states['global']['initial_mass']
-
-
-        import ipdb; ipdb.set_trace()
         
         return {
             'global': {
